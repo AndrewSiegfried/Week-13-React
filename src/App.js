2ecee53fa935463f12911403cbd8869f8025cab6
Andrew Siegfried <Andrewsiegfried1998@gmail.com>
2022-05-09 16:45:09 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Navigation from '../src/component/navbar';
+import Login from '../src/component/loginpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+        <Login/> 
+    </div>
+  );
+}
+
+export default App;
